c2df1752eb23bb807d7c053c9278f438b65b91c9
La svedese <5554930+sarasjodin@users.noreply.github.com>
2025-12-23 07:31:54 +0100

    Delete command implemented to remove the selected todo item

--- WPF-MVVM-ToDo-Project/ViewModels/TodoListViewModel.cs
+++ WPF-MVVM-ToDo-Project/ViewModels/TodoListViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using WPF_MVVM_ToDo_Project.Commands;
 using WPF_MVVM_ToDo_Project.Models;
 
 namespace WPF_MVVM_ToDo_Project.ViewModels
@@ -13,13 +15,30 @@ namespace WPF_MVVM_ToDo_Project.ViewModels
         // Makes list visible on "list view"
         public ObservableCollection<ToDoItem> Todos { get; }
 
+        // Add list logic to utilize select of a todo item (if any)
+        public ToDoItem? SelectedTodo { get; set; }
+
+        // Add command to delete a todo item
+        public ICommand DeleteCommand { get; }
+
         public TodoListViewModel(ObservableCollection<ToDoItem> todos)
         {
             Todos = todos;
+
+            DeleteCommand = new RelayCommand(
+                execute: _ => DeleteSelected(),
+                canExecute: _ => SelectedTodo != null
+            );
         }
 
-        // TODO: Add list logic: selection
+        private void DeleteSelected()
+        {
+            if (SelectedTodo == null) return;
+
+            Todos.Remove(SelectedTodo);   // trigger CollectionChanged from MainViewModel
+            SelectedTodo = null;          // clear selection afterwards
+        }
+
         // TODO: Add list logic: filtering
-        // TODO: Add list logic: commands
     }
 }
